73c3376a2c2181880e599911a5141a6b762b6890
Ryan Braganza <ryan.braganza@gmail.com>
2024-08-22 10:28:48 +1000

    stuff

--- Root.gd
+++ Root.gd
@@ -1,25 +1,26 @@
 extends Node2D
 
-
-
 var lines = []
+@onready var connected_inputs_label: RichTextLabel = %ConnectedInputsLabel
+@onready var midi_events_label: RichTextLabel = %MidiEventsLabel
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	OS.open_midi_inputs()
-	print(OS.get_connected_midi_inputs())
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _process(_delta):
+	# Need to poll the connected midi inputs because they are set asynchronously after open_midi_inputs() is called
+	var connected_inputs = OS.get_connected_midi_inputs()
+	connected_inputs_label.text = 'Connected inputs: %s\n' % connected_inputs.size()
+	connected_inputs_label.text += '\n'.join(connected_inputs)
 
 func _input(input_event):
 	if input_event is InputEventMIDI:
-		_print_midi_info(input_event)
-		lines.append((JSON.stringify(input_event)))
-		lines = lines.slice(-10)
+		print(input_event)
 
-		%RichTextLabel.text = lines.reduce(func(a, b): return a + '\n' + b)
+		midi_events_label.text += str(input_event) + '\n'
+		#lines.append(str(input_event))
+		#lines = lines.slice(-10)
+		#midi_events_label.text = '\n'.join(lines)
 	elif input_event is InputEventKey:
 		if input_event.as_text_keycode() == 'Enter':
 			print('ok')
@@ -28,10 +29,5 @@ func _input(input_event):
 			print('start again')
 			OS.open_midi_inputs()
 
-
-
 func _exit_tree():
 	OS.close_midi_inputs()
-
-func _print_midi_info(midi_event):
-	print(midi_event)
